1f9fbe49fae7b81f5fe337dda8f19932d0335ec6
KishoreT <90593312+KishoreTG@users.noreply.github.com>
2022-02-03 21:51:40 +0530

    Implementation of Doubly Linked List without Behaviour.

--- DataStructures/src/tools/DoublyLinkedList.java
+++ DataStructures/src/tools/DoublyLinkedList.java
@@ -3,11 +3,39 @@ package tools;
 public class DoublyLinkedList {
 
 	// Internal class to represent a Node
+	private class Node {
+		
+		public int value;
+		public Node next;
+		public Node prev;
+		
+		public Node(int value) {
+			this.value = value;
+			this.next = null;
+			this.prev = null;
+		}
+		
+		public Node(int value, Node next, Node prev) {
+			this.value = value;
+			this.next = next;
+			this.prev = prev;
+		}
+		
+	}
 	
 	// Properties
 	// - Reference to Head Node
 	// - Reference to Tail Node
 	// - Size of the List
+	private Node head;
+	private Node tail;
+	private int numOfElems;
+	
+	public DoublyLinkedList() {
+		this.head = null;
+		this.tail = null;
+		this.numOfElems = 0;
+	}
 	
 	// Behaviours
 	// - Get the size of the List
