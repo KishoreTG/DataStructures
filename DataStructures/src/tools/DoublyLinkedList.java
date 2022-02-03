bc90686c1c7a6634b9de78a64e1ad6608e0c474a
KishoreT <90593312+KishoreTG@users.noreply.github.com>
2022-02-03 21:27:28 +0530

    Created a template of Doubly Linked List

---
+++ DataStructures/src/tools/DoublyLinkedList.java
@@ -0,0 +1,25 @@
+package tools;
+
+public class DoublyLinkedList {
+
+	// Internal class to represent a Node
+	
+	// Properties
+	// - Reference to Head Node
+	// - Reference to Tail Node
+	// - Size of the List
+	
+	// Behaviours
+	// - Get the size of the List
+	// - Check whether the List is empty or not
+	// - Add First
+	// - Add Last
+	// - Remove First
+	// - Remove Last
+	// - Insert at
+	// - Remove at
+	// - Check whether a value exists in the List
+	// - Check WHERE the value exists
+	// - toString Representation
+	
+}
